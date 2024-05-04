7d4121cba5f27f8f86ddc9165cd951f7a5860d06
ppenaranda <p.penaranda@uniandes.edu.co>
2024-05-04 08:00:28 -0500

    alarm_url

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', views.index),
     path('', include('measurements.urls')),
     path('', include('variables.urls')),
-    path('health-check/', views.healthCheck)
+    path('health-check/', views.healthCheck),
+    path('', include('alarms.urls')),
 ]
