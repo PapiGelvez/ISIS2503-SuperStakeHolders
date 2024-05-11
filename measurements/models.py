e0c2031f0cc4a99b7b3a235be9bfce1678848fd1
PapiGelvez <s.gelvez@uniandes.edu.co>
2024-05-11 03:01:16 -0500

    prueba1.3

--- measurements/models.py
+++ measurements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import MD5PasswordHasher
+import hashlib
 
 class Measurement(models.Model):
     trabajo = models.CharField(max_length=50, default='')
@@ -11,22 +12,22 @@ class Measurement(models.Model):
     
     def hashearTrabajo(self, trabajo):
         hasher = MD5PasswordHasher() 
-        trabajoHasheado = hasher.encode(self.trabajo, None)
+        trabajoHasheado = hasher.encode(self.trabajo, " ")
         return trabajoHasheado
     
     def hashearIngresos(self, ingresos):
         hasher = MD5PasswordHasher() 
-        ingresosHasheado = hasher.encode(self.ingresos, None)
+        ingresosHasheado = hasher.encode(self.ingresos, " ")
         return ingresosHasheado
     
     def hashearDeudas(self, deudas):
         hasher = MD5PasswordHasher() 
-        deudasHasheado = hasher.encode(self.deudas, None)
+        deudasHasheado = hasher.encode(self.deudas, " ")
         return deudasHasheado
     
     def hashearCreditos(self, creditos):
         hasher = MD5PasswordHasher() 
-        creditosHasheado = hasher.encode(self.creditos, None)
+        creditosHasheado = hasher.encode(self.creditos, " ")
         return creditosHasheado
     
     def save(self, *args, **kwargs):
