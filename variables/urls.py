ad5c3e37ed4921efbf450f41e71aa3e5b7e3b343
ppenaranda <p.penaranda@uniandes.edu.co>
2024-05-11 01:01:31 -0500

    urls variableAdmin

--- variables/urls.py
+++ variables/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('variables/', views.variable_list, name='variableList'),
     path('variablecreate/', csrf_exempt(views.variable_create), name='variableCreate'),
+    path('variablesAdmin/', views.variable_list, name='variableList'),
 ]
