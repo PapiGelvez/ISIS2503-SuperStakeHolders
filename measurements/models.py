214e050de3cce7c6024af78823104c0b68be815f
fpungo <60239261+fpungo@users.noreply.github.com>
2024-05-11 11:19:18 -0500

    ejecucion

--- measurements/models.py
+++ measurements/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class Measurement(models.Model):
-    trabajo = models.CharField(max_length=200, default='')
+    trabajo = models.CharField(max_length=255, default='')
     ingresos = models.CharField(max_length=200, default='')
     deudas = models.CharField(max_length=200, default='')
     creditos = models.CharField(max_length=200, default='')
@@ -14,7 +14,7 @@ class Measurement(models.Model):
         return check_password_hash(valor_hash, str(valor))
 
     def save(self, *args, **kwargs):
-        #self.trabajo = self.cifrar_valor(self.trabajo)
+        self.trabajo = self.cifrar_valor(self.trabajo)
         self.ingresos = self.cifrar_valor(self.ingresos)
         self.deudas = self.cifrar_valor(self.deudas)
         self.creditos = self.cifrar_valor(self.creditos)
